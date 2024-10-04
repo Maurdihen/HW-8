11125e200c31bf7ecae397ae60f9e5cff5dc33e4
egorarhipov <arhipov_0406@mail.ru>
2024-10-04 16:41:38 +0300

    complete

--- src/Author.java
+++ src/Author.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Author {
     private String name;
     private String lastName;
@@ -5,6 +7,28 @@ public class Author {
     public Author(String name, String lastName) {
         this.name = name;
         this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Автор: " + name + " " + lastName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Author author = (Author) o;
+        return Objects.equals(name, author.name) && Objects.equals(lastName, author.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, lastName);
     }
 
     public String getName() {
--- src/Book.java
+++ src/Book.java
@@ -1,7 +1,31 @@
+import java.util.Objects;
+
 public class Book {
     private String title;
     private Author author;
     private int yearPublication;
+
+    @Override
+    public String toString() {
+        return "Книга под названием " + title + " автора " + author + " и " + yearPublication  + " года выпуска";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Book book = (Book) o;
+        return getYearPublication() == book.getYearPublication() && Objects.equals(getTitle(), book.getTitle()) && Objects.equals(getAuthor(), book.getAuthor());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, author, yearPublication);
+    }
 
     public Book(String title, Author author, int yearPublication) {
         this.title = title;
